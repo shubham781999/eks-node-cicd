1ec15de043a490946464bdd1590fe0c05879a127
root <root@ip-172-31-46-166.ap-south-1.compute.internal>
2025-03-07 22:24:27 +0000

    kdskf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('pipeline on 8th of march---------restart'));
+app.get('/', (req, res) => res.send('pipeline on 8th of march rollout restart'));
 app.listen(3000, () => console.log('Server running on port 3000'));
